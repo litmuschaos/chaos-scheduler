438954ebc42fa5bcc1c5878977d9a8074c9cad7c
Shubham Chaudhary <shubham.chaudhary@mayadata.io>
2021-06-15 16:02:39 +0530

    [Cherrypick for 1.13.6] (#14)
    * update(vendor): updating the vendors for the chaosengine (#11)
    
    Signed-off-by: shubhamchaudhary <shubham@chaosnative.com>
    
    * Forward labels and annotations from schedule cr to engines (#13)
    
    * forward labels and annotations from schedule cr to engines
    
    Signed-off-by: Soumya Ghosh Dastidar <gdsoumya@gmail.com>
    
    * removed build binary
    
    Signed-off-by: Soumya Ghosh Dastidar <gdsoumya@gmail.com>
    
    * update(crd): updating scheduler crd and vendor update (#12)
    
    Signed-off-by: shubhamchaudhary <shubham@chaosnative.com>
    
    Co-authored-by: Soumya Ghosh Dastidar <44349253+gdsoumya@users.noreply.github.com>

--- vendor/github.com/litmuschaos/chaos-operator/pkg/apis/litmuschaos/v1alpha1/chaosengine_types.go
+++ vendor/github.com/litmuschaos/chaos-operator/pkg/apis/litmuschaos/v1alpha1/chaosengine_types.go
@@ -36,14 +36,14 @@ type ChaosEngineSpec struct {
 	Components ComponentParams `json:"components"`
 	//Consists of experiments executed by the engine
 	Experiments []ExperimentList `json:"experiments"`
-	//Monitor Enable Status
-	Monitoring bool `json:"monitoring,omitempty"`
 	//JobCleanUpPolicy decides to retain or delete the jobs
 	JobCleanUpPolicy CleanUpPolicy `json:"jobCleanUpPolicy,omitempty"`
 	//AuxiliaryAppInfo contains details of dependent applications (infra chaos)
 	AuxiliaryAppInfo string `json:"auxiliaryAppInfo,omitempty"`
 	//EngineStatus is a requirement for validation
 	EngineState EngineState `json:"engineState"`
+	// TerminationGracePeriodSeconds contains terminationGracePeriod for the chaos resources
+	TerminationGracePeriodSeconds int64 `json:"terminationGracePeriodSeconds,omitempty"`
 }
 
 // EngineState provides interface for all supported strings in spec.EngineState
@@ -68,10 +68,10 @@ const (
 	ExperimentStatusWaiting ExperimentStatus = "Waiting for Job Creation"
 	// ExperimentStatusNotFound is status of Experiment which is not found inside ChaosNamespace
 	ExperimentStatusNotFound ExperimentStatus = "ChaosExperiment Not Found"
-	// ExperimentStatusSuccessful is status of a Successful experiment execution
-	ExperimentStatusSuccessful ExperimentStatus = "Execution Successful"
 	// ExperimentStatusAborted is status of a Experiment is forcefully aborted
 	ExperimentStatusAborted ExperimentStatus = "Forcefully Aborted"
+	// ExperimentSkipped is status of Experiment which has been skipped
+	ExperimentSkipped ExperimentStatus = "Skipped"
 )
 
 // EngineStatus provides interface for all supported strings in status.EngineStatus
@@ -140,7 +140,7 @@ type RunnerInfo struct {
 	//ImagePullSecrets for runner pod
 	ImagePullSecrets []corev1.LocalObjectReference `json:"imagePullSecrets,omitempty"`
 	// Runner Annotations that needs to be provided in the pod for pod that is getting created
-	RunnerAnnotation map[string]string `json:"runnerannotation,omitempty"`
+	RunnerAnnotation map[string]string `json:"runnerAnnotations,omitempty"`
 	// NodeSelector for runner pod
 	NodeSelector map[string]string `json:"nodeSelector,omitempty"`
 	// ConfigMaps for runner pod
@@ -193,9 +193,6 @@ type ProbeAttributes struct {
 	// mode for k8s probe
 	// it can be SOT, EOT, Edge
 	Mode string `json:"mode,omitempty"`
-	// Operation performed by the k8s probe
-	// it can be create, delete, present, absent
-	Operation string `json:"operation,omitempty"`
 	// Data contains the manifest/data for the resource, which need to be created
 	// it supported for create operation only
 	Data string `json:"data,omitempty"`
@@ -203,14 +200,6 @@ type ProbeAttributes struct {
 
 // K8sProbeInputs contains all the inputs required for k8s probe
 type K8sProbeInputs struct {
-	// Command need to be executed for the probe
-	Command K8sCommand `json:"command,omitempty"`
-	// Expected output or result of the command
-	ExpectedResult string `json:"expectedResult,omitempty"`
-}
-
-// K8sCommand contains all the commands need for the k8sprobe
-type K8sCommand struct {
 	// group of the resource
 	Group string `json:"group,omitempty"`
 	// apiversion of the resource
@@ -223,6 +212,9 @@ type K8sCommand struct {
 	FieldSelector string `json:"fieldSelector,omitempty"`
 	// labelselector to get the resource using labels selector
 	LabelSelector string `json:"labelSelector,omitempty"`
+	// Operation performed by the k8s probe
+	// it can be create, delete, present, absent
+	Operation string `json:"operation,omitempty"`
 }
 
 //CmdProbeInputs contains all the inputs required for cmd probe
@@ -267,8 +259,40 @@ type HTTPProbeInputs struct {
 	URL string `json:"url,omitempty"`
 	// InsecureSkipVerify flag to skip certificate checks
 	InsecureSkipVerify bool `json:"insecureSkipVerify,omitempty"`
-	// Expected response code from the given url
-	ExpectedResponseCode string `json:"expectedResponseCode,omitempty"`
+	// Method define the http method, it can be get or post
+	Method HTTPMethod `json:"method,omitempty"`
+	// ResponseTimeout contains the http response timeout
+	ResponseTimeout int `json:"responseTimeout,omitempty"`
+}
+
+// HTTPMethod define the http method details
+type HTTPMethod struct {
+	Get  GetMethod  `json:"get,omitempty"`
+	Post PostMethod `json:"post,omitempty"`
+}
+
+// GetMethod define the http Get method
+type GetMethod struct {
+	// Criteria for matching data
+	// it supports  == != operations
+	Criteria string `json:"criteria,omitempty"`
+	// Value contains relative value for criteria
+	ResponseCode string `json:"responseCode,omitempty"`
+}
+
+// PostMethod define the http Post method
+type PostMethod struct {
+	// ContentType contains content type for http body data
+	ContentType string `json:"contentType,omitempty"`
+	// Body contains http body for post request
+	Body string `json:"body,omitempty"`
+	// BodyPath contains filePath, which contains http body
+	BodyPath string `json:"bodyPath,omitempty"`
+	// Criteria for matching data
+	// it supports  == != operations
+	Criteria string `json:"criteria,omitempty"`
+	// Value contains relative value for criteria
+	ResponseCode string `json:"responseCode,omitempty"`
 }
 
 //RunProperty contains timeout, retry and interval for the probe
@@ -284,6 +308,10 @@ type RunProperty struct {
 	ProbePollingInterval int `json:"probePollingInterval,omitempty"`
 	//InitialDelaySeconds time interval for which probe will wait before run
 	InitialDelaySeconds int `json:"initialDelaySeconds,omitempty"`
+	// StopOnFailure contains flag to stop/continue experiment execution, if probe fails
+	// it will stop the experiment execution, if provided true
+	// it will continue the experiment execution, if provided false or not provided(default case)
+	StopOnFailure bool `json:"stopOnFailure,omitempty"`
 }
 
 // ExperimentComponents contains ENV, Configmaps and Secrets
@@ -291,7 +319,7 @@ type ExperimentComponents struct {
 	ENV                        []corev1.EnvVar               `json:"env,omitempty"`
 	ConfigMaps                 []ConfigMap                   `json:"configMaps,omitempty"`
 	Secrets                    []Secret                      `json:"secrets,omitempty"`
-	ExperimentAnnotations      map[string]string             `json:"experimentannotation,omitempty"`
+	ExperimentAnnotations      map[string]string             `json:"experimentAnnotations,omitempty"`
 	ExperimentImage            string                        `json:"experimentImage,omitempty"`
 	ExperimentImagePullSecrets []corev1.LocalObjectReference `json:"experimentImagePullSecrets,omitempty"`
 	NodeSelector               map[string]string             `json:"nodeSelector,omitempty"`
